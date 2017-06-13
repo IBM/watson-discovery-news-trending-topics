d1f979d1c1a22b30d5019560e7485c9e60fd7c42
Ankur Patel <ankur.patel@ymail.com>
2017-06-13 11:38:13 -0400

    Adding tests

--- server/query-builder.test.js
+++ server/query-builder.test.js
@@ -1,13 +1,13 @@
 import queryBuilder from './query-builder';
 import moment from 'moment';
 
-describe('Query builder', () => {
-  beforeEach(() => {
-    queryBuilder.setCollectionId('collection');
-    queryBuilder.setEnvironmentId('environment');
-  });
+beforeEach(() => {
+  queryBuilder.setCollectionId('collection');
+  queryBuilder.setEnvironmentId('environment');
+});
 
-  test('returns params for discovery service when opts are not passed', () => {
+describe('Query builder returns params for discovery service', () => {
+  test('when opts are NOT passed', () => {
     expect(queryBuilder.build()).toEqual({
       environment_id: 'environment',
       collection_id: 'collection',
@@ -16,6 +16,20 @@ describe('Query builder', () => {
         'term(enrichedTitle.entities.text,count:20).top_hits(1)'
       ],
       filter: `blekko.chrondate>${moment().subtract(24,'h').unix()},blekko.hostrank>300`
+    });
+  });
+
+  test('when opts are passed', () => {
+    expect(queryBuilder.build({
+      filter: 'taxonomy.label:"test"'
+    })).toEqual({
+      environment_id: 'environment',
+      collection_id: 'collection',
+      return: 'enrichedTitle.entities.text',
+      aggregations: [
+        'term(enrichedTitle.entities.text,count:20).top_hits(1)'
+      ],
+      filter: `taxonomy.label:"test",blekko.chrondate>${moment().subtract(24,'h').unix()},blekko.hostrank>300`
     });
   });
 });
